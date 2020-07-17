41ff915347654a5c20f6e024a2e4fa6e2ae160f7
rooler <roel.wier@gmail.com>
2020-07-17 13:23:58 -0400

    updated XXZ data

--- change_names.sh
+++ change_names.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+
+for x in *; do
+   mv "$x" "${x//XXZ_8qb_delta_/}";
+done
+for x in *; do
+   mv "$x" "${x//_closed_af/}";
+done
+
 for d in $(find . -maxdepth 1 -type d)
 do
 echo $d
-  Do something, the directory is accessible with $d:
-  mv -- "$d/qaoa_grid_2_stats.txt" "$d/stats.txt"
-  mv -- "$d/qaoa_grid_2_params.npy" "$d/params.npy"
-  mv -- "$d/qaoa_grid_2_final_state.npy" "$d/final_state.npy"
-  mv -- "$d/qaoa_grid_2_energies.npy" "$d/energies.npy"
+  mv -- "$d/qaoa_grid_4_stats.txt" "$d/stats.txt"
+  mv -- "$d/qaoa_grid_4_params.npy" "$d/params.npy"
+  mv -- "$d/qaoa_grid_4_final_state.npy" "$d/final_state.npy"
+  mv -- "$d/qaoa_grid_4_energies.npy" "$d/energies.npy"
 done
 
-#for x in *; do
-#   echo $x
-#   mv "$x" "${x//XXZ_4qb_delta_/}";
-#   mv "$x" "${x//_closed_af/}";
-#done
